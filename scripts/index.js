52c00807828c8d0c1306680d7c2408e32ab29183
Luís Nascimento <luisrocksousa1111@gmail.com>
2024-12-22 23:54:18 -0300

    reestruturando pastas

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,5 @@
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__celular');
-const button = document.querySelector('.hamburguer');
-
-button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
-});
 
 window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
